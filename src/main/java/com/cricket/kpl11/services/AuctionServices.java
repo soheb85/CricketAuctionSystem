f71ff08e6a5d92adc49756c97b3345d72b9e40dd
soheb85 <shoebkhan3145@gmail.com>
2025-02-11 22:32:04 +0530

    Added GenerateRandom class and generate all unique player

--- src/main/java/com/cricket/kpl11/services/AuctionServices.java
+++ src/main/java/com/cricket/kpl11/services/AuctionServices.java
@@ -5,11 +5,11 @@ import com.cricket.kpl11.dto.GeneratePlayerDto;
 import com.cricket.kpl11.entity.Players;
 import com.cricket.kpl11.mapper.AuctionMapper;
 import com.cricket.kpl11.repository.PlayersRepo;
+import com.cricket.kpl11.utility.GenerateRandom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @Service
 public class AuctionServices {
@@ -17,19 +17,29 @@ public class AuctionServices {
     @Autowired
     private PlayersRepo playersRepo;
 
-    private Random random = new Random();
+    private Set<Long> alreadyGenerated = new HashSet<>();
 
     // ---->> Generate Random Player for auction.
     public GeneratePlayerDto sendPlayer(){
-        List<Players> generatePlayer = playersRepo.findByIsSoldFalse();
-        if(generatePlayer.isEmpty()){
-            throw new RuntimeException("No Player Available to Generate");
+        if (alreadyGenerated.size() >= 4) { // If all 4 numbers are used, throw an exception
+            throw new RuntimeException("All players have been generated, no more players left.");
         }
-        Players sendPlayers = generatePlayer.get(random.nextInt(generatePlayer.size()));
-        return AuctionMapper.mapToGeneratePlayerDto(sendPlayers);
+
+        long randomNumber = GenerateRandom.generateRandom();
+        while(alreadyGenerated.contains(randomNumber)){
+
+                randomNumber = GenerateRandom.generateRandom();
+
+        }
+        alreadyGenerated.add(randomNumber);
+
+        Players generatePlayer = playersRepo.findById(randomNumber)
+                .orElseThrow(()-> new RuntimeException("Player not Found by ID"));
+
+        return AuctionMapper.mapToGeneratePlayerDto(generatePlayer);
     }
 
-    // ------>> Search player by Id and send generatePlayerDto.
+    // ------>> Search player by I'd and send generatePlayerDto.
 
 
 
---
+++ src/main/java/com/cricket/kpl11/utility/GenerateRandom.java
@@ -0,0 +1,13 @@
+package com.cricket.kpl11.utility;
+
+import java.util.Random;
+
+public class GenerateRandom {
+
+    private static  final Random random = new Random();
+
+    public static long generateRandom(){
+
+        return 1+random.nextInt(4);
+    }
+}
